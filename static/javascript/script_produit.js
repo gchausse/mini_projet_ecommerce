c445f2809b8855030db99515d10de39e59f083c6
guillaume <guillaume.chausse@live.fr>
2017-03-17 10:13:13 +0100

    remplacement de catalog.js par function get ajax

--- static/javascript/script_produit.js
+++ static/javascript/script_produit.js
@@ -1,26 +1,37 @@
 $(document).ready(function(){
-    var index = GET_PARAM("index")
-    var produit = catalog[index]
-    $("h2").html(catalog[index].name);
-    $("#prix").html(catalog[index].price);
-    $("#description").html(catalog[index].description);
-    $("img").attr('src', catalog[index].thumb)
+    var catalog = {}
 
-    var count = 0;
-    $("#ajoutpanier").click(function(){
-        var panier_json = localStorage.getItem("objet");
-        var panier = JSON.parse(panier_json);
-        if(panier === undefined){
-        var panier = {};
-        }
-        count ++;
-        panier[index]=count
-        console.log(panier);
+    $.get (
+        'https://codi-e-commerce.herokuapp.com/',
+        'false',
+        retour,
+        'json'
+    );
+    function retour (x){
+        catalog  = x;
+        var index = GET_PARAM("index")
+        var produit = catalog[index]
+        $("h2").html(catalog[index].name);
+        $("#prix").html(catalog[index].price);
+        $("#description").html(catalog[index].description);
+        $("img").attr('src', catalog[index].thumb)
 
-        var panier_json = JSON.stringify(panier);
-        localStorage.setItem("objet",panier_json);
-        var panier_json = localStorage.getItem("objet");
-        var panier = JSON.parse(panier_json);
-    })
+        var count = 0;
+        $("#ajoutpanier").click(function(){
+            var panier_json = localStorage.getItem("objet");
+            var panier = JSON.parse(panier_json);
+            if(panier === undefined){
+                var panier = {};
+            }
+            count ++;
+            panier[index]=count
+            console.log(panier);
+
+            var panier_json = JSON.stringify(panier);
+            localStorage.setItem("objet",panier_json);
+            var panier_json = localStorage.getItem("objet");
+            var panier = JSON.parse(panier_json);
+        })
+    }
 
 })
